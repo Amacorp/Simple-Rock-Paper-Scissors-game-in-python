9572e5edde7804e69422f6ed1c616e1e369b4453
Mekhdi Gakhramanian <37830907+Amacorp@users.noreply.github.com>
2020-06-29 19:59:27 +0430

    Create play.py

---
+++ play.py
@@ -0,0 +1,21 @@
+def rps(p1, p2):
+    beats = {'rock': 'scissors', 'scissors': 'paper', 'paper': 'rock'}
+    if beats[p1] == p2:
+        return "Player 1 won!"
+    if beats[p2] == p1:
+        return "Player 2 won!"
+    return "Draw!"
+    
+or
+
+
+def rps(p1, p2):
+    hand = {'rock':0, 'paper':1, 'scissors':2}
+    results = ['Draw!', 'Player 1 won!', 'Player 2 won!']
+    return results[hand[p1] - hand[p2]]
+    
+    
+or
+
+
+rps = lambda a, b: ['Draw!', 'Player 1 won!', 'Player 2 won!'][('srp'.index(a[0]) - 'srp'.index(b[0])) % 3]
